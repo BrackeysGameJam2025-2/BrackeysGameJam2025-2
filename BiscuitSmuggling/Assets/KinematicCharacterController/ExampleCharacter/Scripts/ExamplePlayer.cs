2a298b5ef0288832835a5755b1b6916f98fd83c2
markustar <marcustarian69@gmail.com>
2025-08-26 20:07:22 +0200

    Make player movement again and make that camera doesn't rotate

---
+++ BiscuitSmuggling/Assets/KinematicCharacterController/ExampleCharacter/Scripts/ExamplePlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KinematicCharacterController;
+using KinematicCharacterController.Examples;
+
+namespace KinematicCharacterController.Examples
+{
+    public class ExamplePlayer : MonoBehaviour
+    {
+        public ExampleCharacterController Character;
+
+        private const string HorizontalInput = "Horizontal";
+        private const string VerticalInput = "Vertical";
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+            HandleCharacterInput();
+        }
+
+        private void HandleCharacterInput()
+        {
+            PlayerCharacterInputs characterInputs = new PlayerCharacterInputs();
+
+            // Build the CharacterInputs struct
+            characterInputs.MoveAxisForward = Input.GetAxisRaw(VerticalInput);
+            characterInputs.MoveAxisRight = Input.GetAxisRaw(HorizontalInput);
+            characterInputs.CameraRotation = Quaternion.identity; // No camera rotation
+            characterInputs.JumpDown = Input.GetKeyDown(KeyCode.Space);
+            characterInputs.CrouchDown = Input.GetKeyDown(KeyCode.C);
+            characterInputs.CrouchUp = Input.GetKeyUp(KeyCode.C);
+
+            // Apply inputs to character
+            Character.SetInputs(ref characterInputs);
+        }
+    }
+}
