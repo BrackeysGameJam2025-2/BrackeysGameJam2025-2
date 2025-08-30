c62c4b0b914c9f1948d93d879f041bc967d6c993
Ohotnik <volodyaohana@gmail.com>
2025-08-30 16:31:36 +0200

    Fridge interaction

--- BiscuitSmuggling/Assets/Scripts/InteractiveObject/FridgeInteraction.cs
+++ BiscuitSmuggling/Assets/Scripts/InteractiveObject/FridgeInteraction.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FridgeInteraction", menuName = "Scriptable Objects/InteraciveObjectBechavior/Fridge")]
 public class FridgeInteraction : InteractiveObjectBehavior
 {
+    [SerializeField]
+    private ItemMetadata food;
     public override void Prepare(DialogBehaviour dialogBehaviour)
     {
-
+        if (Inventory.Instance.HasItem(food, 1))
+            dialogBehaviour.SetVariableValue("haveFood", true);
+        else
+            dialogBehaviour.SetVariableValue("haveFood", false);
     }
 
     public override void Accept()
     {
         TriggerInteractionResult(true);
+        Inventory.Instance.AddItem(food, 1);
     }
 
     public override void Reject()
